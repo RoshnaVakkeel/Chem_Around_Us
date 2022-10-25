87bf429b2f3c3a61f6d5a07a00d59f272fdfa4fd
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2022-10-25 13:21:08 +0000

    add validation to username input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
+let usernameForm = document.getElementById("username-form");
+let usernameButton = document.getElementById("username-submit");
 
+usernameButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let username = usernameForm.username.value;
 
-
-
+    if (username.length < 3) {
+        alert("Username must be atleast 3 charactrs long, Please Try Again!");
+    } else {
+        alert("Your name is saved!!");
+    }
+})
