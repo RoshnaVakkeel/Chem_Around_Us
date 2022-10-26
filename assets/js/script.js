52db202f14304bdfb7cc9f8a756d93770b6c652b
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2022-10-26 10:26:40 +0000

    add list of questions and  function to js to load questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,46 @@ usernameButton.addEventListener("click", (e) => {
         alert("Your name is saved!!");
     }
 })
+
+//Questions list for quiz-box (arrays)
+
+let question = document.getElementById('question');
+
+let questionList = [{
+        question: 'What makes over 99% of the matter in the universe?',
+        option: ['Helium and Hydrogen', 'Nitrogen and Hydrogen', 'Oxygen and Nitrogen', 'Oxygen and Hydrogen'],
+        answer: 'Helium and Hydrogen'
+    },
+    {
+        question: 'What is ozone layer made of?',
+        option: ['Dioxygen', 'Trioxygen', 'Oxygen and Hydrogen', 'Methane gas'],
+        answer: 'Trioxygen'
+    },
+]
+
+
+/**
+ * Loads and updates questions in the quiz-box
+ */
+
+let quizBox = document.getElementById('quiz-box');
+let questionArea = document.getElementById ('question-area')
+let spanAll = document.querySelectorAll('span');
+console.log (spanAll)
+
+let option0 = document.getElementById('option0');
+let option1 = document.getElementById('option1');
+let option2 = document.getElementById('option2');
+let option3 = document.getElementById('option3');
+let i = 0;
+
+function loadQuestion() {
+    for (var a=0; a<spanAll.length; a++) {
+        spanAll[a];
+    }
+    question.innerHTML = (i + 1) + '.'+'     '+ questionList[i].question;
+    option0.innerHTML = questionList[i].option[0];
+    option1.innerHTML = questionList[i].option[1];
+    option2.innerHTML = questionList[i].option[2];
+    option3.innerHTML = questionList[i].option[3];
+}
