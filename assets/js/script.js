ae4ab4ce0ebb123da53f854bd840629eb629abe6
RoshnaVakkeel <vakkeel.roshna@gmail.com>
2022-10-27 00:19:53 +0000

    add function to next button and update the quiz-box with new questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,15 +24,31 @@ usernameButton.addEventListener("click", (e) => {
  * Save user's name to the 
  */
 function saveUserData() {
-    let name = document.getElementById ("username-field").value;
+    let name = document.getElementById("username-field").value;
     console.log("users value is: " + name);
- }
+}
 
 /**
  * btn-next / Let's Begin button condition
  * so that it doesn't work if username is not entered.
  */
+let buttonStart = document.getElementById("btn-start");
+let introArea = document.getElementById ('intro-area')
 
+function btnStart() {
+    quizBox.style.display = 'block';
+    introArea.style.display = 'none';
+
+    buttonStart.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        if (`$name` == null || `$name` == "") {
+            alert("Please enter your Username!");
+        } else {
+            throw ("Aborting!");
+        }
+    })
+}
 
 //Questions list for quiz-box (arrays)
 
@@ -48,6 +64,12 @@ let questionList = [{
         option: ['Dioxygen', 'Trioxygen', 'Oxygen and Hydrogen', 'Methane gas'],
         answer: 'Trioxygen'
     },
+    {
+        question: 'Diamond is made of which element?',
+        option: ['Alloy of phosphorus and carbon', 'Carbon and phosphorus', 'Phosphorous', 'Carbon'],
+        answer: 'Carbon'
+    },
+
 ]
 
 
@@ -56,9 +78,9 @@ let questionList = [{
  */
 
 let quizBox = document.getElementById('quiz-box');
-let questionArea = document.getElementById ('question-area')
+let questionArea = document.getElementById('question-area')
 let spanAll = document.querySelectorAll('span');
-console.log (spanAll)
+console.log(spanAll)
 
 let option0 = document.getElementById('option0');
 let option1 = document.getElementById('option1');
@@ -67,30 +89,41 @@ let option3 = document.getElementById('option3');
 let i = 0;
 
 function loadQuestion() {
-    for (var a=0; a<spanAll.length; a++) {
+    for (let a = 0; a < spanAll.length; a++) {
         spanAll[a];
     }
-    question.innerHTML = (i + 1) + '.'+'     '+ questionList[i].question;
+    question.innerHTML = (i + 1) + '.' + '     ' + questionList[i].question;
     option0.innerHTML = questionList[i].option[0];
     option1.innerHTML = questionList[i].option[1];
     option2.innerHTML = questionList[i].option[2];
     option3.innerHTML = questionList[i].option[3];
 }
 
+/**
+ * Function to change the questions and display the next
+ */
+ 
+ let buttonNext= document.getElementById ('btn-next');
+ let resultArea = document.getElementById ('result-area');
+ let score = document.getElementById('score');
+
+ function nextQuestion() {
+    if (i < questionList.length - 1) {
+        i = i + 1;
+        loadQuestion();
+    } else {
+        quizBox.style.display = 'none';
+        score.innerHTML = score + '/'+ questionList.length;
+        resultArea.style.display = 'block'
+    }
+}
+
 loadQuestion()
+
 
 /**
  * Function to check answers and calculate scores
  */
 
- let score = document.getElementById ('score');
- 
- function calcScore(e){
-    
- }
-
-/**
- * Function to change the questions abd display the next
- */
 
 
